665ca848029b169a250699f3da9fd9f907d2c183
ism417 <ielab036@gmail.com>
2023-12-23 13:15:34 +0100

    update
    update fix memory leak

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: eismail <eismail@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 09:22:41 by eismail           #+#    #+#             */
-/*   Updated: 2023/12/23 12:53:44 by eismail          ###   ########.fr       */
+/*   Updated: 2023/12/23 13:12:13 by eismail          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		if (!new_node)
 		{
 			del(content);
-			ft_lstclear(&new_node, del);
+			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
 		ft_lstadd_back(&new_list, new_node);
